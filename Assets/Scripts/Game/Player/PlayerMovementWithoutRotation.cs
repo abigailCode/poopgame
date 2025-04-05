171e582ac12204c887c06138ecb9cb6c348b7614
CarlosSuaRod <suarezcarlos641@gmail.com>
2025-04-05 13:50:06 +0100

    InputSystem y PlayerController

--- Assets/Scripts/Game/Player/PlayerMovementWithoutRotation.cs
+++ Assets/Scripts/Game/Player/PlayerMovementWithoutRotation.cs
@@ -1,148 +1,117 @@
-using System.Collections;
 using System;
-using System.Collections.Generic;
 using UnityEngine;
-using System.Collections.Specialized;
+using UnityEngine.InputSystem;
 
-public class PlayerMovementWithoutRotation : MonoBehaviour
-{
-    public float horizontalMove;
-    public float verticalMove;
+public class PlayerMovementWithoutRotation : MonoBehaviour {
     public CharacterController player;
-
     public float speed;
 
     private Vector3 playerInput;
+    private Vector3 movePlayer;
 
     public Camera mainCamera;
-
     private Vector3 camForward;
     private Vector3 camRight;
-
-    private Vector3 movePlayer;
 
     public float gravity = 9.8f;
     public float fallVelocity;
     public float jumpForce;
 
     private float lastDamageTime;
-    public float damageInterval = 1f; // Intervalo de tiempo entre decrementos de HP
+    public float damageInterval = 1f;
 
     public GameObject levelController;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    [SerializeField] InputActionReference inputMove;
+
+    private void OnEnable() {
+        inputMove.action.Enable();
+    }
+
+    private void OnDisable() {
+        inputMove.action.Disable();
+    }
+
+    void Start() {
         player = GetComponent<CharacterController>();
         lastDamageTime = -damageInterval;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        horizontalMove = Input.GetAxis("Horizontal");
-        verticalMove = Input.GetAxis("Vertical");
+    void Update() {
+        Vector3 input = inputMove.action.ReadValue<Vector3>();
 
-        playerInput = new Vector3(horizontalMove, 0, verticalMove);
+        // Movimiento en XZ
+        playerInput = new Vector3(input.x, 0, input.z);
         playerInput = Vector3.ClampMagnitude(playerInput, 1);
 
         CamDirection();
 
         movePlayer = playerInput.x * camRight + playerInput.z * camForward;
+        movePlayer *= speed;
 
-        movePlayer = movePlayer * speed;
+        // Rotar hacia la dirección del movimiento
+        if (movePlayer != Vector3.zero)
+            player.transform.LookAt(player.transform.position + new Vector3(movePlayer.x, 0, movePlayer.z));
 
-        player.transform.LookAt(player.transform.position + movePlayer);
-
+        // Aplicar gravedad
         SetGravity();
 
-        if (Input.GetButtonDown("Jump") && player.isGrounded)
-        {
+        // Saltar si input.y > 0 y está en el suelo
+        if (input.y > 0.1f && player.isGrounded) {
             SetJump();
         }
 
+        // Mover al personaje
         player.Move(movePlayer * Time.deltaTime);
-
-       // Debug.Log(player.velocity.magnitude);
     }
 
-    void CamDirection()
-    {
+    void CamDirection() {
         camForward = mainCamera.transform.forward;
         camRight = mainCamera.transform.right;
 
         camForward.y = 0;
         camRight.y = 0;
 
-        camForward = camForward.normalized;
-        camRight = camRight.normalized;
+        camForward.Normalize();
+        camRight.Normalize();
     }
 
-    void SetGravity()
-    {
-        //if (player.isGrounded)
-        //{
-        //    fallVelocity = -gravity * Time.deltaTime;
-        //    movePlayer.y = fallVelocity;
-        //}
-        //else
-        //{
-        //    fallVelocity -= gravity * Time.deltaTime;
-        //    movePlayer.y = fallVelocity;
-        //}
+    void SetGravity() {
         if (player.isGrounded) {
             fallVelocity = -gravity * Time.deltaTime - 0.0001f;
-            movePlayer.y = fallVelocity; //magic number
-        } else
-        {
+        } else {
             fallVelocity -= gravity * Time.deltaTime;
-            movePlayer.y = fallVelocity;
         }
+        movePlayer.y = fallVelocity;
     }
 
-    void SetJump()
-    {
-       
+    void SetJump() {
         fallVelocity = jumpForce;
         movePlayer.y = fallVelocity;
-        
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Enemy")
-        {
-                mainCamera.GetComponent<CameraShake>().Shake(0.5f, 0.7f);
-            AudioManager.Instance.PlaySFX("damage");
-            float hp = GameObject.Find("Pointer").GetComponent<HealthBarBehaviour>().GetHp();
-            GameObject.Find("Pointer").GetComponent<HealthBarBehaviour>().DecrementHp(20f);
-        }
-        else if (other.tag == "Water")
-        {
-            AudioManager.Instance.PlaySFX("water");
-            GameObject.Find("Pointer").GetComponent<HealthBarBehaviour>().IncrementHp(15f);
-            Destroy(other.transform.parent.gameObject);
-        }else if(other.tag == "PickUp")
-        {
-            AudioManager.Instance.PlaySFX("pickup");
-            levelController.GetComponent<LevelManager>().IncrementCounter(1);
-            Destroy(other.transform.parent.gameObject);
-        }
+    //private void OnTriggerEnter(Collider other) {
+    //    if (other.CompareTag("Enemy")) {
+    //        mainCamera.GetComponent<CameraShake>().Shake(0.5f, 0.7f);
+    //        AudioManager.Instance.PlaySFX("damage");
+    //        GameObject.Find("Pointer").GetComponent<HealthBarBehaviour>().DecrementHp(20f);
+    //    } else if (other.CompareTag("Water")) {
+    //        AudioManager.Instance.PlaySFX("water");
+    //        GameObject.Find("Pointer").GetComponent<HealthBarBehaviour>().IncrementHp(15f);
+    //        Destroy(other.transform.parent.gameObject);
+    //    } else if (other.CompareTag("PickUp")) {
+    //        AudioManager.Instance.PlaySFX("pickup");
+    //        levelController.GetComponent<LevelManager>().IncrementCounter(1);
+    //        Destroy(other.transform.parent.gameObject);
+    //    }
+    //}
 
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.tag == "Enemy")
-        {
-            if (Time.time - lastDamageTime >= damageInterval)
-            {
-                    mainCamera.GetComponent<CameraShake>().Shake(0.5f, 0.7f);
-                AudioManager.Instance.PlaySFX("damage");
-                lastDamageTime = Time.time;
-                float hp = GameObject.Find("Pointer").GetComponent<HealthBarBehaviour>().GetHp();
-                GameObject.Find("Pointer").GetComponent<HealthBarBehaviour>().DecrementHp(5f);
-            }
-        }
-    }
+    //private void OnTriggerStay(Collider other) {
+    //    if (other.CompareTag("Enemy") && Time.time - lastDamageTime >= damageInterval) {
+    //        mainCamera.GetComponent<CameraShake>().Shake(0.5f, 0.7f);
+    //        AudioManager.Instance.PlaySFX("damage");
+    //        lastDamageTime = Time.time;
+    //        GameObject.Find("Pointer").GetComponent<HealthBarBehaviour>().DecrementHp(5f);
+    //    }
+    //}
 }
